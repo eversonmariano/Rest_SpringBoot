4182fdc5164546af7cef9cac69cb6dfaf8c8855c
eversonmariano <mariano.computacao@gmail.com>
2021-11-02 14:08:13 -0300

    SpringBoot Custom Serialization

---
+++ SpringBoot Custom Serialization/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person`
+VALUES (1, 'campina grande', 'ell', 'male', 'mariano'),
+       (2, 'joao pessoa', 'maria', 'female', 'batista'),
+       (3, 'natal', 'laura', 'female', 'paula'),
+       (4, 'recife', 'paulo', 'male', 'henrique')
