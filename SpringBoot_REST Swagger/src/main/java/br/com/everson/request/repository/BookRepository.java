41f72e9365f1a42d14445d3cff884585b0b3f0a0
eversonmariano <mariano.computacao@gmail.com>
2021-11-12 18:45:02 -0300

    Adding Swagger suport

---
+++ SpringBoot_REST Swagger/src/main/java/br/com/everson/request/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package br.com.everson.request.repository;
+
+import br.com.everson.data.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
